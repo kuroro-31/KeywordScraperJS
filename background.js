ea1a7458cdafd38737c194fd095bd227ea2907c1
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2025-01-30 05:03:14 +0900

    バグ修正

--- background.js
+++ background.js
@@ -1,36 +1,5 @@
 // background.js
 
-// Q&A/ブログ/SNS ドメイン定義（Python版のロジックを移植）
-const QA_SITES = [
-  "detail.chiebukuro.yahoo.co.jp",
-  "chiebukuro.yahoo.co.jp",
-  "okwave.jp",
-  "oshiete.goo.ne.jp",
-  "komachi.yomiuri.co.jp",
-];
-
-const BLOG_SITES = [
-  "blog.hatena.ne.jp",
-  "ameblo.jp",
-  "plaza.rakuten.co.jp",
-  "note.com",
-  "fanblogs.jp",
-  "cocolog-nifty.com",
-  "tumblr.com",
-  "fc2.com",
-  "blog.goo.ne.jp",
-  "https://plaza.rakuten.co.jp",
-];
-
-const SNS_SITES = {
-  Tiktok: ["tiktok.com"],
-  Instagram: ["instagram.com"],
-  X: ["x.com", "twitter.com"],
-  Facebook: ["facebook.com"],
-  Youtube: ["youtube.com"],
-  Twitch: ["twitch.tv"],
-};
-
 // 分析対象のキーワードリスト
 let keywordQueue = [];
 let currentIndex = 0;
